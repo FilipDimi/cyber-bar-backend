067477c0c04da05a80aea66c2f6732ce52acbb6e
dimitr36 <dimitr36@msu.edu>
2023-06-29 11:34:35 -0400

    Query for displaying current tab

--- core/schema.py
+++ core/schema.py
@@ -2,15 +2,15 @@ import graphene
 import graphql_jwt
 from django.contrib.auth import get_user_model
 from graphql_jwt.decorators import login_required
-from core.types import BarCategoryType, BarSubCategoryType, BeverageType, UserType, CocktailType
-from core.models import BarCategory, BarSubCategory, Beverage, Cocktail
+from core.types import BarCategoryType, BarSubCategoryType, BeverageType, UserType, CocktailType, BarTabItemType
+from core.models import BarCategory, BarSubCategory, Beverage, Cocktail, BarArchive, BarTabItem
 from core.mutations import CreateUser, AddItemToTab
+from datetime import datetime
 
 User = get_user_model()
 
 
 class Mutation(graphene.ObjectType):
-    # token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
@@ -43,6 +43,9 @@ class Query(graphene.ObjectType):
     def resolve_current_user(root, info, **kwargs):
         user = info.context.user
         return user
+    
+    # Tabs
+    user_tab = graphene.Field(BarTabItemType, user_pk=graphene.String(required=True))
 
     # Categories
     def resolve_all_barcategories(root, info):
@@ -80,6 +83,13 @@ class Query(graphene.ObjectType):
     
     def resolve_search_cocktail(root, info, id):
         return Cocktail.objects.get(pk=id)
+    
+    # Tabs
+    def resolve_user_tab(root, info, user_pk):
+        temp_archive = BarArchive.objects.get(date=str(datetime.today().strftime('%Y-%m-%d')))
+        temp_user = User.objects.get(pk=user_pk)
+        tab_items = BarTabItem.objects.filter(date=temp_archive, user=temp_user)
+        return tab_items
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
